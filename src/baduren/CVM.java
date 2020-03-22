89f29c11fbd8094e52ef47c81c907653b493c5ad
zarhof <cbardoux26@gmail.com>
2020-03-22 18:53:29 +0100

    Let's continue test integration

--- src/baduren/CVM.java
+++ src/baduren/CVM.java
@@ -213,19 +213,19 @@ public class CVM  extends AbstractCVM {
 			// Create an instance of the defined component virtual machine.
 			CVM a = new CVM() ;
 			// Execute the application.
-			a.startStandardLifeCycle(1000L) ;
+			a.startStandardLifeCycle(5000L) ;
 			System.out.println("Messages publiées à partir de Publisher: "+ PublisherTeacher.publications);
 			System.out.println("Messages sauvegardés dans broker : " + Broker.messagesSupprimes);
 			System.out.println("Historique des abonnements dans broker : "+ Broker.historiqueAbonnements);
 			System.out.println("Historique des changement de filtres  dans broker : "+Broker.changementFiltres);
 			System.out.println("Historique des desabonnements dans broker : "+ Broker.desabonnements);
-			System.out.println("Historique des création de topic dans broker: "+ Broker.historiqueCreationTopics);
+			/*System.out.println("Historique des création de topic dans broker: "+ Broker.historiqueCreationTopics);
 			System.out.println("Historique des destruction de topics dans broker: "+ Broker.suppressionSujets);
 			System.out.println("Réponse de la demande 'isTopic(ALASCA)' dans publisher (doit retourner vrai)" +
 					": "+ PublisherTeacher.reponseIsTopic);
 			System.out.print("Tous les sujets après avoir supprimé les topics : ");
-			for (String s :  SubscriberStudent.allTopicsAtTheEnd)
-				System.out.print(s+"   ");
+			for (String s :  SubscriberStudent.allTopicsAtTheEnd)*/
+				//System.out.print(s+"   ");
 			System.out.println("");
 			// Give some time to see the traces (convenience).
 			Thread.sleep(5000L) ;
